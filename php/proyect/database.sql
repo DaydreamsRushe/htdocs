5386759536ef4b99e3ec5f07aa19ab93178ece51
JoseLuisOrtega <jlop9810@gmail.com>
2025-05-21 19:37:55 +0200

    database final and header menu

--- php/proyect/database.sql
+++ php/proyect/database.sql
@@ -1,4 +1,4 @@
--- Active: 1747734032471@@127.0.0.1@3306@usuarisrole
+-- Active: 1741626660969@@127.0.0.1@3306@ejercicioprofe
 DROP DATABASE IF EXISTS serenalive;
 
 CREATE DATABASE  serenalive DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -15,43 +15,45 @@ CREATE TABLE usuario (
 
 CREATE TABLE paciente (
     user_id INT PRIMARY KEY,
-    nombre_paciente VARCHAR(50),
+    nombre_paciente VARCHAR(50) NOT NULL,
     FOREIGN KEY (user_id) REFERENCES usuario(id)
 )ENGINE=InnoDB;
 
 CREATE TABLE profesional (
     user_id INT PRIMARY KEY,
-    nombre_profesional VARCHAR(50),
+    nombre_profesional VARCHAR(50) NOT NULL,
     FOREIGN KEY (user_id) REFERENCES usuario(id)
 )ENGINE=InnoDB;
 
-CREATE TABLE farmaco (
-    nombre_farmaco VARCHAR(50) PRIMARY KEY, 
-    proveedor VARCHAR(50) NOT NULL
-)ENGINE=InnoDB;
-
-CREATE TABLE dolencia (
-    nombre_dolencia VARCHAR(50) PRIMARY KEY,
+CREATE TABLE diagnostico (
+    id_diagnostico INT PRIMARY KEY AUTO_INCREMENT,
+    nombre_dolencia VARCHAR(50) NOT NULL,
     farmaco VARCHAR(50),
-    FOREIGN KEY (farmaco) REFERENCES farmaco(nombre_farmaco)
+    FOREIGN KEY (nombre_dolencia) REFERENCES enfermedad(nombre_enfermedad)
 )ENGINE=InnoDB;
 
 CREATE TABLE especializa (
     id_profesional INT,
-    dolencia VARCHAR(50),
+    especialidad VARCHAR(50),
     PRIMARY KEY (id_profesional, dolencia),
     FOREIGN KEY (id_profesional) REFERENCES profesional(user_id),
-    FOREIGN KEY (dolencia) REFERENCES dolencia(nombre_dolencia)
+    FOREIGN KEY (especialidad) REFERENCES enfermedad(nombre_enfermedad)
 )ENGINE=InnoDB;
 
-CREATE TABLE terapia (
+CREATE TABLE enfermedad (
+  nombre_enfermedad VARCHAR(50) PRIMARY KEY,
+  nombre_farmaco VARCHAR(50)
+)ENGINE=InnoDB;
+
+/* Todo paciente tendra solo un profesional asociado, pero un profesional puede tener varios */
+CREATE TABLE visita (
     id_paciente INT,
     id_profesional INT,
-    dolencia VARCHAR(50),
+    diagnostico INT,
     fecha DATE NOT NULL,
     PRIMARY KEY (id_profesional, id_paciente, fecha),
     FOREIGN KEY (id_profesional) REFERENCES profesional(user_id),
     FOREIGN KEY (id_paciente) REFERENCES paciente(user_id),
-    FOREIGN KEY (dolencia) REFERENCES dolencia(nombre_dolencia),
-    CONSTRAINT professional_date UNIQUE (id_profesional, fecha)
+    FOREIGN KEY (diagnostico) REFERENCES diagnostico(id_diagnostico),
+    CONSTRAINT professional_date UNIQUE (id_profesional, fecha),
 )ENGINE=InnoDB;
